1bcbd553f0a29e9c24eb2c8c2a4ee47715e4cde2
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-06-12 10:50:45 +0900

    簡単なAPIサーバー

--- src/go.mod
+++ src/go.mod
@@ -1,5 +1,3 @@
 module E-Commerce-Chat-Microservice
 
 go 1.22
-
-require github.com/gorilla/mux v1.8.0
--- src/main.go
+++ src/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-    "fmt"
+    "encoding/json"
     "net/http"
-    "github.com/gorilla/mux"
 )
 
-func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
-    })
-
-    http.ListenAndServe(":8080", r)
+type Response struct {
+    Message string `json:"message"`
 }
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    response := Response{Message: "Hello, World!"}
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
+func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+    response := Response{Message: "Goodbye, World!"}
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+    http.HandleFunc("/", helloHandler)
+    http.HandleFunc("/goodbye", goodbyeHandler)
+    http.ListenAndServe(":8080", nil)
+}
+
